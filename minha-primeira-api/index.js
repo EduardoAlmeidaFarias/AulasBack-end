0234178941dcf3206168a5843bdd04e7c3beb4b1
Eduardo de Almeida <jacquesdasoul@gmail.com>
2026-08-26 22:24:25 -0300

    criando primeiras rotas

--- minha-primeira-api/index.js
+++ minha-primeira-api/index.js
@@ -1,3 +1,9 @@
 import express from 'express'
 
-const app = express();
+const app = express(); //primeiro pilar instancia do express
+
+app.get("/hello", (req, res) => { //segundo pilar rotas
+  res.send("Hello World!");
+});
+
+app.listen(3000); //terceiro pilar: porta a ser ouvida
